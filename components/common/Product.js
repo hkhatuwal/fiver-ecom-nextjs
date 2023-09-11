5925fe03dac02da27d52da1e5ebdb7fca3ff5800
Himanshu <prjapathimanshu51@gmail.com>
2023-09-11 16:17:50 +0530

    fixed iamge broder

--- components/common/Product.js
+++ components/common/Product.js
@@ -6,8 +6,8 @@ import Link from "next/link";
 const Product = ({slug,image,title,price,rating,hideRating=false,hidePrice=false}) => {
     return (
        <Link href={"/product/"+slug}>
-           <div  className={'flex justify-center items-center flex-col cursor-pointer '}>
-               <Image className={'w-full p-6  aspect-square'} src={image} alt={""} width={300} height={300}/>
+           <div  className={'flex justify-center items-center flex-col cursor-pointer p-2 '}>
+               <Image className={'w-full   aspect-square object-contain border-2 border-black rounded-[2.2rem] p-5 my-1'} src={image} alt={""} width={300} height={300}/>
                <h3 className={`text-3xl font-extrabold ${hidePrice?'hidden':''}` }>${price}</h3>
                <h4 className={'text-xl font-bold '}>{title}</h4>
                <div className={`flex gap-1 ${hideRating?'hidden':''}`}>
